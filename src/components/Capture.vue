e0a38ff17cd7b2da1451d3a0e00f37efe7a33f71
Andrej Kyselica <andrej@kyselica.com>
2019-02-18 17:22:28 -0600

    Toggle capture/view UI; single camera swap button

--- src/components/Capture.vue
+++ src/components/Capture.vue
@@ -1,59 +1,63 @@
 <template>
-<div>
-  <v-flex xs12 mb-5>
-    <web-cam ref="webcam"
-                   :device-id="deviceId"
-                   width="100%"
-                   @started="onStarted" 
-                   @stopped="onStopped" 
-                   @error="onError"
-                   @cameras="onCameras"
-                   @camera-change="onCameraChange" />
-  </v-flex>
-   <div>
-      <select v-model="camera">
-              <option>-- Select Device --</option>
-              <option v-for="device in devices" 
-                      :key="device.deviceId" 
-                      :value="device.deviceId">{{ device.label }}</option>
-            </select>
-            
-            <button type="button" 
-                    class="btn btn-primary" 
-                    @click="onCapture">Capture Photo</button>
-            <button type="button" 
-                    class="btn btn-danger" 
-                    @click="onStop">Stop Camera</button>
-            <button type="button" 
-                    class="btn btn-success" 
-                    @click="onStart">Start Camera</button>
-          </div>
+  <div>
+    <div v-show="inCaptureMode">
+      <v-flex xs12 mb-5>
+        <web-cam
+          ref="webcam"
+          :device-id="deviceId"
+          width="100%"
+          @started="onStarted"
+          @stopped="onStopped"
+          @error="onError"
+          @cameras="onCameras"
+          @camera-change="onCameraChange"
+        />
+      </v-flex>
+      <div>
+        <select v-model="camera">
+          <option>-- Select Device --</option>
+          <option
+            v-for="device in devices"
+            :key="device.deviceId"
+            :value="device.deviceId"
+          >{{ device.label }}</option>
+        </select>
 
-    <h2>Captured Image</h2>
-        <figure class="figure">
-          <img :src="img" class="img-responsive" >
-        </figure>
-        </div>
+        <v-btn color="info" @click="onCapture">Capture Photo</v-btn>
+        <!-- <v-btn color="success" @click="onStop">Stop Camera</v-btn>
+        <v-btn color="danger" @click="onStart">Start Camera</v-btn> -->
+        <v-btn @click="onNextDevice">Next Device</v-btn>
+      </div>
+    </div>
+    <div v-show="!inCaptureMode">
+      <h2>Captured Image</h2>
+      <figure class="figure">
+        <img :src="img" class="img-responsive">
+      </figure>
+      <v-btn color="info" @click="onClearCapture">Clear Capture</v-btn>
+    </div>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import WebCam from 'vue-web-cam'
-import { find, head } from "lodash";
+import Vue from "vue";
+import WebCam from "vue-web-cam";
+import { find, head, findIndex } from "lodash";
 
-console.log(WebCam)
-const wc = Vue.component(WebCam.WebCam.name, WebCam.WebCam)
+console.log(WebCam);
+const wc = Vue.component(WebCam.WebCam.name, WebCam.WebCam);
 
 export default {
   components: {
-    'WebCam': wc
+    WebCam: wc
   },
   data() {
     return {
       img: null,
       camera: null,
       deviceId: null,
-      devices: []
+      devices: [],
+      inCaptureMode: true
     };
   },
   computed: {
@@ -77,6 +81,10 @@ export default {
   methods: {
     onCapture() {
       this.img = this.$refs.webcam.capture();
+      this.inCaptureMode = false;
+    },
+    onClearCapture() {
+      this.inCaptureMode = true;
     },
     onStarted(stream) {
       console.log("On Started Event", stream);
@@ -101,11 +109,21 @@ export default {
       this.deviceId = deviceId;
       this.camera = deviceId;
       console.log("On Camera Change Event", deviceId);
+    },
+    onNextDevice() {
+      if (this.devices && this.devices.length) {
+        console.log(this);
+        let thisIndex =
+          findIndex(this.devices, d => d.deviceId === this.deviceId) + 1;
+        if (thisIndex >= this.devices.length) thisIndex = 0;
+        const { deviceId } = this.devices[thisIndex];
+        this.deviceId = deviceId;
+        this.camera = deviceId;
+      }
     }
   }
 };
 </script>
 
 <style lang="less">
-
 </style>
