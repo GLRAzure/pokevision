0b0e70fc246384f53c1692e393b83ec110c699da
Andrej Kyselica <andrej@Kyselica.com>
2019-02-20 21:21:15 -0500

    initial working version

--- src/azureCustomVision.js
+++ src/azureCustomVision.js
@@ -4,24 +4,27 @@
  * @param {string} predictionKey
  * @returns function that takes an image binary and returns a prediction json
  */
-export function getPredictionClient(predictionEndpointUri, predictionKey) {
+export function getPredictionClient(predictionEndpointUri, appId, predictionKey) {
   // set up headers for the requests
   const headers = new Headers({
-    ContentType: "application/octet-stream",
-    "Prediction-Key": predictionKey
+    // ContentType: "application/octet-stream",
+    "Prediction-Key": predictionKey,
+    "Content-Type": "multipart/form-data"
   });
 
-  return async function prediect(image) {
+  const predictionEndpointFullUri = `${predictionEndpointUri}/${appId}/image`
+
+  return async function predict(image) {
     const response = await fetch(
-      new Request(predictionEndpointUri, {
+      new Request(predictionEndpointFullUri, {
         method: "POST",
         headers,
         body: image
       })
     );
-
+    // debugger;
     if (response.status == 200) {
-      return JSON.parse(response.body);
+      return await response.json();
     } else {
       throw new Error(response.statusText);
     }
--- src/components/PokeInfo.vue
+++ src/components/PokeInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-show="img">Image received</div>
-    <div v-show="pokemonName">Your Pokemon is {{pokemonName}}</div>
+    <div>Your Pokemon is {{pokemonName}}</div>
   </div>
 </template>
 
@@ -10,30 +10,50 @@ import { getPredictionClient } from "../azureCustomVision";
 
 const client = getPredictionClient(
   "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/",
+  "426cf8d2-6977-41a0-8752-40465286c030",
   "a504cd822ef2404f85e63a505d8d5f0f"
 );
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
 
 export default {
   data: function() {
     return {
-      pokemonName: null,
-      img: null
+      pokemonName: null
     };
   },
   watch: {
     img: async function(newVal) {
       console.log("watch:img");
-      const response = await client(newVal);
+      const response = await client(dataURItoBlob(newVal));
       console.log("got response", response);
+      if (response.predictions && response.predictions.length) {
+        console.log("getting top response");
+        const topResponse = response.predictions.sort((a,b) => b.probability - a.probability)[0];
+        console.log("Top response:", topResponse);
+        this.pokemonName = topResponse.tagName;
+      }
     }
   },
-  props: ["image"],
-  methods: {
-    async setPokemonImage(img) {
-      const response = await client(img);
-      console.log("got response", response);
-    }
-  }
+  props: ["img"]
 };
 </script>
 
